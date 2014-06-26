9b52fe9905930e9c05a4c0a901ad8dbed8b3e016
DieterDoman <dieter.doman@gmail.com>
2014-06-26 13:32:43 +0200

    Started working on server to handle post request

--- ServerSide/ServerModule/Jetty/src/example/server/MinimalServer.java
+++ ServerSide/ServerModule/Jetty/src/example/server/MinimalServer.java
@@ -1,32 +1,68 @@
 package example.server;
-
+ 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
  
-public class MinimalServer {
- 
-    public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
+public class MinimalServer 
+{
+	private static Server server = null;
+	
+    public static void main(String[] args) throws Exception 
+    {
+    	//Create server
+        server = new Server(8080);
         ServletHandler handler = new ServletHandler();
-        handler.addServletWithMapping(HelloServlet.class, "/hello");//Set the servlet to run.
+        handler.addServletWithMapping(LoginServlet.class, "/login");//Set the servlet to run.
         server.setHandler(handler);    
         server.start();
         server.join();
     }
  
     @SuppressWarnings("serial")
-    public static class HelloServlet extends HttpServlet {
- 
+    public class LoginServlet extends HttpServlet 
+    {
         @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            response.setContentType("text/html");
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().println("<h1>Hello SimpleServlet</h1>");
+        //Handle post requests
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+        {
+        	//Get post parameters
+            String user = request.getParameter("user");
+            String id = request.getParameter("id");
+            
+            
+            //login successful need to do database with user id's and plug in with Emma dummy
+            if (user.equals("test")== true)
+            {
+            	JSONObject jsonResponse = new JSONObject();
+            	try 
+            	{
+            		jsonResponse.put("Status", "Success");
+            		jsonResponse.put("SessionId", server.getSessionIdManager());
+				} 
+            	catch (JSONException e) 
+            	{
+					e.printStackTrace();
+				}
+            	
+            	//need to respond with post with json object
+            }
+            //Login fails
+            else
+            {
+            	
+            }
+            //System.out.println(user + " " + id);
         }
+            
     }
 }
