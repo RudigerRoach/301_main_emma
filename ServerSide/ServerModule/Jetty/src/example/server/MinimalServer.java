0954808d475b9af3153ea98306b5e7218ebf2423
DieterDoman <dieter.doman@gmail.com>
2014-07-01 14:30:56 +0200

    Started on unit test for login with junit and modified some of the server code

--- ServerSide/ServerModule/Jetty/src/example/server/MinimalServer.java
+++ ServerSide/ServerModule/Jetty/src/example/server/MinimalServer.java
@@ -9,21 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
  
 public class MinimalServer 
 {
-	private static Server server = null;
 	
     public static void main(String[] args) throws Exception 
     {
     	//Create server
-        server = new Server(8080);
+        Server server = new Server(8080);
         ServletHandler handler = new ServletHandler();
         handler.addServletWithMapping(LoginServlet.class, "/login");//Set the servlet to run.
-        server.setHandler(handler);    
+        server.setHandler(handler);
         server.start();
         server.join();
     }
@@ -36,33 +34,51 @@ public class MinimalServer
         protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
         {
         	//Get post parameters
-            String user = request.getParameter("user");
-            String id = request.getParameter("id");
+            String user = request.getParameter("email");
+            String id = request.getParameter("deviceUID");
             
-            
-            //login successful need to do database with user id's and plug in with Emma dummy
-            if (user.equals("test")== true)
+            //test if no parameters are sent
+            if(user != null)
             {
-            	JSONObject jsonResponse = new JSONObject();
-            	try 
+            	//login successful need to do database with user id's and plug in with Emma dummy
+            	if ((user).equals("test")== true)
             	{
-            		jsonResponse.put("Status", "Success");
-            		jsonResponse.put("SessionId", server.getSessionIdManager());
-				} 
-            	catch (JSONException e) 
-            	{
-					e.printStackTrace();
-				}
-            	
-            	//need to respond with post with json object
-            }
-            //Login fails
+            		JSONObject jsonResponse = new JSONObject();
+            		try 
+	            	{
+	            		jsonResponse.put("status", "success");
+	            		//SessionID manage
+	            		jsonResponse.put("session_id", "123");
+					} 
+	            	catch (JSONException e) 
+	            	{
+						e.printStackTrace();
+					}
+	            	
+            		//response to successful login
+	            	response.setContentType("application/json;charset=UTF-8");
+	            	response.getWriter().print(jsonResponse.toString());
+	            }
+	            //Login fails
+	            else
+	            {
+	            	JSONObject jsonResponse = new JSONObject();
+	            	try 
+	            	{
+	            		jsonResponse.put("status", "failed");
+					} 
+	            	catch (JSONException e) 
+	            	{
+						e.printStackTrace();
+					}
+	            	
+	            	//response to failed login
+	            	response.setContentType("application/json;charset=UTF-8");
+	            	response.getWriter().print(jsonResponse.toString());
+	            }
+	        }
             else
-            {
-            	
-            }
-            //System.out.println(user + " " + id);
+            	System.out.println("Parameter is null..");
         }
-            
     }
 }
---
+++ ServerSide/ServerModule/Jetty/src/example/server/MinimalServerTest.java
@@ -0,0 +1,91 @@
+package example.server;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+@SuppressWarnings("deprecation")
+public class MinimalServerTest 
+{
+	private static Server server = null;
+	
+	@Before
+	public void testSetup() throws Exception
+	{
+		server = new Server(8080);
+        server.setStopAtShutdown(true);
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/");
+        server.setHandler(context);
+        MinimalServer testServer = new MinimalServer();
+        MinimalServer.LoginServlet testLoginServlet = testServer.new LoginServlet();
+        context.addServlet(new ServletHolder(testLoginServlet),"/login");
+        server.start();
+	}
+
+	@After
+	public void testCleanup() throws Exception 
+	{
+		server.stop();
+	}
+
+	@Test
+	public void testLoginSuccessful() throws Exception
+	{
+			HttpClient client = new DefaultHttpClient();
+	        HttpPost mockRequest = new HttpPost("http://localhost:8080/login");
+	        mockRequest.setHeader("Content-type", "application/x-www-form-urlencoded");
+	        
+	        List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+	        urlParameters.add(new BasicNameValuePair("email", "test"));
+	        urlParameters.add(new BasicNameValuePair("deviceUID", "123"));
+	        mockRequest.setEntity(new UrlEncodedFormEntity(urlParameters,"UTF-8"));
+	        HttpResponse mockResponse = client.execute(mockRequest);
+	        
+	        JSONObject jsonTest = new JSONObject();
+	        jsonTest.put("status", "success");
+			jsonTest.put("session_id", "123");
+			
+			BufferedReader rd = new BufferedReader(new InputStreamReader(mockResponse.getEntity().getContent()));
+	        assertEquals("Testing if login was succesful",jsonTest.toString(),rd.readLine());
+    }
+	
+	@Test
+	public void testLoginFailure() throws Exception
+	{
+		HttpClient client = new DefaultHttpClient();
+        HttpPost mockRequest = new HttpPost("http://localhost:8080/login");
+        mockRequest.setHeader("Content-type", "application/x-www-form-urlencoded");
+        
+        List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+        urlParameters.add(new BasicNameValuePair("email", "asdf"));
+        urlParameters.add(new BasicNameValuePair("deviceUID", "123"));
+        mockRequest.setEntity(new UrlEncodedFormEntity(urlParameters,"UTF-8"));
+        HttpResponse mockResponse = client.execute(mockRequest);
+        
+        JSONObject jsonTest = new JSONObject();
+        jsonTest.put("status", "failed");
+		
+		BufferedReader rd = new BufferedReader(new InputStreamReader(mockResponse.getEntity().getContent()));
+        assertEquals("Testing if login was correctly failed due to incorrect username",jsonTest.toString(),rd.readLine());
+	}
+}
