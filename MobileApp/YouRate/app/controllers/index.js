8bd3dd4184bf5c083a8ca32f03d02207684156b8
Melanybarnes <searcher@searchers-Mac.local>
2014-07-12 04:48:06 -0700

    doLogin function added

--- MobileApp/YouRate/app/controllers/index.js
+++ MobileApp/YouRate/app/controllers/index.js
@@ -1,13 +1,21 @@
 function displayLoginPage(e){
 	//Call autoLogin
-	
+	var service=require('authentication');
+	service.autoLogin();
+	var success = service.loginStatus();
 	//If autoLogin successful
-    //var win=Alloy.createController('vote').getView();
- 	//win.open();
-	
-	//If autoLogin not successful
-    var win=Alloy.createController('login').getView();
- 	win.open();
+	if (success == true)
+	{
+		//The user is allowed to use the rest of the application, thus display the next page
+		var win=Alloy.createController('vote').getView();
+	 	win.open();
+	}
+	else
+	{	
+		//If autoLogin not successful
+	    var win=Alloy.createController('login').getView();
+	 	win.open();
+ 	}
 };
 
 $.startPage.open();
