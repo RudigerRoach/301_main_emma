3dd25bf24139f58a2b39b6e1bc05fd530d23c812
RudigerRoach <RudigerRoach@gmail.com>
2014-07-12 02:29:41 -0700

    Improved login unit test.

--- MobileApp/YouRate/app/lib/specs/lib/authentication.js
+++ MobileApp/YouRate/app/lib/specs/lib/authentication.js
@@ -1,16 +1,37 @@
 require("/tijasmine/tijasmine").infect(this);
 
+
+
 describe("Authentication tests", function() {
-	
+
 	var auth = require('Authentication');
+	var goodTime = 10000; //The network call should exicute within 10 seconds
+	
 	it("login", function() {
-		var status = auth.login('test@mail.com');
-		expect(status).toBe(true);
+		var status = false;
+		var flag = false;
+		
+		runs(function(){
+			auth.login('test@mail.com');
+			setTimeout(function() {
+				flag = true;
+			}, goodTime-100);
+			status = auth.loginStatus;
+		});
+		
+		waitsFor(function() {
+			return flag;
+		}, "The call timed out", goodTime);
+		
+		
+		runs(function(){
+			expect(status).toBe(true);
+		});
+	});
+	
+	it("autoLogin", function() {
+		expect(auth.autoLogin()).toBe(true);
+		
 	});
   
-  it("autoLogin", function() {
-    expect(auth.autoLogin()).toBe(true);
-  });
-  
 });
-
