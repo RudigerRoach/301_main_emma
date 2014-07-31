80332f9213a1f1e798f2e6b3d71200957dc57649
RudigerRoach <RudigerRoach@gmail.com>
2014-07-31 06:29:16 -0700

    Refactored Network module

--- MobileApp/YouRate/app/lib/Network.js
+++ MobileApp/YouRate/app/lib/Network.js
@@ -5,9 +5,14 @@
 var XHR = require("xhr");
 var xhr = new XHR();
 
-var netAddress = 'http://192.168.0.101:5555';
-var LoginURL= netAddress+'/login';
-var getImgURL=netAddress+'/start';
+var netAddress = 'http://192.168.0.100';
+var port = "5555";
+var LoginURL= netAddress+':'+port+'/login';
+var getImgURL=netAddress+':'+port+'/start';
+
+exports.serverPath = function(){
+	return netAddress;
+};
 
 exports.loginPost = function(payload , onSuccessCallback, onErrorCallback){
 	xhr.post(LoginURL, payload , onSuccessCallback, onErrorCallback);
