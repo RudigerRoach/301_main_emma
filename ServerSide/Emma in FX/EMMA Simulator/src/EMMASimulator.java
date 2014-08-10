ac56db407f87e83ba9b9a49fa5aad5641bf3b901
JohanEsterhuyse <dj.esterhuyse@ymail.com>
2014-08-10 22:23:33 +0200

    Working FX Class uploaded

--- ServerSide/Emma in FX/EMMA Simulator/src/EMMASimulator.java
+++ ServerSide/Emma in FX/EMMA Simulator/src/EMMASimulator.java
@@ -1,14 +1,13 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -16,6 +15,17 @@ import javafx.stage.Stage;
  * @author Endzeit
  */
 public class EMMASimulator extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("Emma Simulator");
+        stage.show();
+    }
 
     /**
      * @param args the command line arguments
@@ -24,23 +34,4 @@ public class EMMASimulator extends Application {
         launch(args);
     }
     
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
 }
---
+++ ServerSide/Emma in FX/EMMA Simulator/src/FXMLDocumentController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author Endzeit
+ */
+public class FXMLDocumentController implements Initializable{
+     /**
+     * Initializes the controller class.
+     */
+    
+    @FXML
+    private Button start;
+   
+    public FXMLDocumentController()
+    {
+        
+    }
+    
+    @FXML
+    private void handleStartButtonAction(ActionEvent event) throws IOException {
+        System.out.println("You clicked me!");
+//        Parent root = FXMLLoader.load(getClass().getResource("FXMLrunningSession.fxml"));
+//        Scene newScene = new Scene(FXMLLoader.load(getClass().getResource("FXMLrunningSession.fxml")));
+//        Stage newStage = new Stage();
+//        newStage.setScene(newScene);
+//        newStage.setTitle("Emma Simulator");
+//        newStage.show();
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+}
