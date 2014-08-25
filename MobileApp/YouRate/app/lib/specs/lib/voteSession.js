cffda5c993ea170963be85117f9294bea51d5f8f
RudigerRoach <RudigerRoach@gmail.com>
2014-08-25 02:52:40 -0700

    Next Image Request unit tests complete and runs successfully

--- MobileApp/YouRate/app/lib/specs/lib/voteSession.js
+++ MobileApp/YouRate/app/lib/specs/lib/voteSession.js
@@ -5,7 +5,7 @@ describe("VoteSession start of session test", function() {
 	var voteSession = require('VoteSession');
 	var goodTime = 10000;
 	//The network call should exicute within 10 seconds
-	it("login status", function() {
+	it("service status", function() {
 		function testStatus(service) {
 			var done = false;
 			var timer = setInterval(function() {
@@ -102,6 +102,72 @@ describe("VoteSession start of session test", function() {
 				done = service.getImgDone();
 				if (done) {
 					expect(service.commentsEnabled()).toBe(true);
+					clearInterval(timer);
+				}
+			}, goodTime);
+		}
+		voteSession.getImage();
+		testStatus(voteSession);
+	});
+});
+
+describe("Next image request tests", function() {
+
+	var voteSession = require('VoteSession');
+	var goodTime = 10000;
+	//The network call should exicute within 10 seconds
+	it("service status", function() {
+		function testStatus(service) {
+			var done = false;
+			var timer = setInterval(function() {
+				done = service.getImgDone();
+				if (done) {
+					expect(service.status()).toBe("1"); //doResults(service);// 
+					clearInterval(timer);
+				}
+			}, goodTime);
+		}
+		voteSession.getImage();
+		testStatus(voteSession);
+	});
+	
+	it("session type", function() {
+		function testStatus(service) {
+			var done = false;
+			var timer = setInterval(function() {
+				done = service.getImgDone();
+				if (done) {
+					expect(service.sessionType()).toBe("normal");
+					clearInterval(timer);
+				}
+			}, goodTime);
+		}
+		voteSession.getImage();
+		testStatus(voteSession);
+	});
+	
+	it("image path", function() {
+		function testStatus(service) {
+			var done = false;
+			var timer = setInterval(function() {
+				done = service.getImgDone();
+				if (done) {
+					expect(service.imagePath()).toBe("testPath");
+					clearInterval(timer);
+				}
+			}, goodTime);
+		}
+		voteSession.getImage();
+		testStatus(voteSession);
+	});
+	
+	it("description", function() {
+		function testStatus(service) {
+			var done = false;
+			var timer = setInterval(function() {
+				done = service.getImgDone();
+				if (done) {
+					expect(service.description()).toBe("testDetails");
 					clearInterval(timer);
 				}
 			}, goodTime);
