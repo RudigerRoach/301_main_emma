58b698b4533b43586e12fbb9b5776af043529637
RudigerRoach <RudigerRoach@gmail.com>
2014-10-06 07:00:22 -0700

    fixed slider not updating bug

--- MobileApp/YouRate/app/controllers/normal.js
+++ MobileApp/YouRate/app/controllers/normal.js
@@ -2,10 +2,12 @@ var ui = require('ui');
 var service = require('VoteSession');
 var isIOS = ui.isIOS();
 var updateSlider = false;
+var firstload = true;
+//a boolean that will ONLY be true up untill the page has completed drawing for the first time
 
 //declare variables and use defaults for testing
 var commentButton = null;
-var commentText = "";	
+var commentText = "";
 var rangeBottom = 0;
 var rangeTop = 50;
 var description = "Image title";
@@ -14,114 +16,109 @@ var imagePath = "/universal/placeholder.png";
 //imagePath = ospath+"animalLandscape.jpg";
 
 //Server calls
- rangeBottom = service.rangeBottom();
- rangeTop = service.rangeTop();
- description = service.description();
- comments = service.commentsEnabled();
- imagePath = service.imagePath();
+rangeBottom = service.rangeBottom();
+rangeTop = service.rangeTop();
+description = service.description();
+comments = service.commentsEnabled();
+imagePath = service.imagePath();
 
 //Bind event listeners to the slider and score input box to make them play nice
-$.slider.addEventListener('change', function(e) {
-	if(!updateSlider)
-	{
-		$.sliderArea.value = Math.floor(Number($.slider.value));
-	}
-	else
-	{
+$.scoreSlider.addEventListener('change', function(e) {
+	if (!updateSlider) {
+		$.sliderArea.value = Math.floor(Number($.scoreSlider.value));
+	} else {
 		updateSlider = false;
 	}
 });
 $.sliderArea.addEventListener('return', function(e) {
 	updateSlider = true;
-	$.slider.value = Math.floor(Number($.sliderArea.value));;
+	newValue = Math.floor(Number($.sliderArea.value));
+	$.scoreSlider.value = newValue;
 	$.sliderArea.blur();
 });
 
 //Resize all artifacts on the screen to match the screen size and orientation
-function resizePage()
-{
+function resizePage() {
 	var screenWidth = ui.platformWidth();
 	var screenHeight = ui.platformHeight();
 	if (!isIOS) {
 		screenHeight -= 60;
 	}
 	var screenLeft = screenHeight;
-	
+
 	$.submitButton.top = screenHeight - 70;
-	$.submitButton.width = screenWidth - 40;	
+	$.submitButton.width = screenWidth - 40;
 	screenLeft = $.submitButton.top;
-	
-	if(comments == "true")
-	{
-		if(commentButton == null)
-		{
+
+	if (comments == "true") {
+		if (commentButton == null) {
 			commentsEnabled();
 			$.sliderArea.blur();
 		}
 		commentButton.top = screenLeft - 70;
-	    commentButton.width = screenWidth - 40;
-	    screenLeft = commentButton.top;
+		commentButton.width = screenWidth - 40;
+		screenLeft = commentButton.top;
 	}
-	
+
 	$.sliderLabel.top = screenLeft - 50;
-	$.sliderLabel.right = screenWidth/2 + 10;
-	
+	$.sliderLabel.right = screenWidth / 2 + 10;
+
 	$.sliderArea.top = screenLeft - 50;
-	$.sliderArea.left = screenWidth/2 + 20;
+	$.sliderArea.left = screenWidth / 2 + 20;
 	screenLeft = $.sliderLabel.top;
-	
-	$.slider.top = screenLeft - 40;
-	$.slider.min = rangeBottom;
-	$.slider.max = rangeTop;
-	$.slider.width = screenWidth - 40;
-	$.slider.value = (rangeBottom+rangeTop)/2;	
-    $.sliderArea.value = $.slider.value;
-	screenLeft = $.slider.top;
-	
-	if(!isIOS){
+
+	$.scoreSlider.top = screenLeft - 40;
+	if (firstload) {
+		$.scoreSlider.min = rangeBottom;
+		$.scoreSlider.max = rangeTop;
+		$.scoreSlider.value = (rangeBottom + rangeTop) / 2;
+		$.sliderArea.value = $.scoreSlider.value;
+		$.currentImage.image = imagePath;
+		firstload = false;
+	}
+
+	$.scoreSlider.width = screenWidth - 40;
+	screenLeft = $.scoreSlider.top;
+
+	if (!isIOS) {
 		$.currentImage.top = 30;
 	}
-	$.currentImage.image = imagePath;
+
 	$.currentImage.height = screenLeft - 80;
 	$.currentImage.width = "auto";
 }
 
-function doSubmit(e)
-{	
-	//Submit result	
-	service=require('VoteSession');	
-	service.submitResult(Math.floor(Number($.slider.value)),commentText);
+function doSubmit(e) {
+	//Submit result
+	service = require('VoteSession');
+	service.submitResult(Math.floor(Number($.scoreSlider.value)), commentText);
 	//service.submitResult(photosView.currentPage,"");
-    //alert("Result successfully submitted");
-    
-   
-    //Go to wait page
-	var win=Alloy.createController('wait').getView();
- 	win.open();
+	//alert("Result successfully submitted");
+
+	//Go to wait page
+	var win = Alloy.createController('wait').getView();
+	win.open();
 }
 
 function commentsEnabled() {
-		commentButton = ui.getCommentButton();
-		commentArea = ui.getCommentArea();
-	
-		commentArea.addEventListener('setFocus',function(e){
-			commentArea.focus();
-		});
-		commentButton.addEventListener('click',function(e)
-		{
-				$.normalPage.add(commentArea);
-				commentArea.fireEvent('setFocus');
-		});
-		commentArea.addEventListener('blur',function(e)
-		{
-			commentText = commentArea.value;
-			commentArea.blur();
-			$.normalPage.remove(commentArea);
-		});
-		commentArea.addEventListener('return',function(e)
-		{
-			commentArea.blur();
-		});
+	commentButton = ui.getCommentButton();
+	commentArea = ui.getCommentArea();
+
+	commentArea.addEventListener('setFocus', function(e) {
+		commentArea.focus();
+	});
+	commentButton.addEventListener('click', function(e) {
+		$.normalPage.add(commentArea);
+		commentArea.fireEvent('setFocus');
+	});
+	commentArea.addEventListener('blur', function(e) {
+		commentText = commentArea.value;
+		commentArea.blur();
+		$.normalPage.remove(commentArea);
+	});
+	commentArea.addEventListener('return', function(e) {
+		commentArea.blur();
+	});
 	$.normalPage.add(commentButton);
 }
 
@@ -129,3 +126,4 @@ Ti.Gesture.addEventListener('orientationchange', function(e) {
 	resizePage();
 });
 $.normalPage.open();
+$.normalPage.focus();
